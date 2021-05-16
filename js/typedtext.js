b35a13d0e77d3ac4fd3bb3848d84829429335bf6
casperwang <casperwang2014@gmail.com>
2021-05-16 21:22:05 +0800

    typewriter

--- js/typedtext.js
+++ js/typedtext.js
@@ -1,16 +1,32 @@
-const speed = 100;
+const speed = 80;
+var target = [$("#quote"), $("#handle")];
+var target_idx = 0;
+var content = [$("#quote").text(), $("#handle").text()];
+var content_idx = 0;
 
-function typewriter(target, content, idx) {
-	var display_content = string(content).substring(0, idx);
-
-	if (idx == content.length) {
-		target.text(display_content);
-	} else {
-		target.text(display_content + "_");
-		setTimeout(typewriter(target, idx+1), speed);
+function init() {
+	for (var i = 0; i < target.length; i++) {
+		target[i].text("");
 	}
 }
 
+function typewriter() {
+	var display_content = String(content[target_idx]).substring(0, content_idx);
 
-typewriter($("#quote"), $("#quote").text(), 0);
-typewriter($("#handle"), $("#handle").text(), 0);
+	if (content_idx == content[target_idx].length) {
+		target[target_idx++].text(display_content);
+		if (target_idx == target.length) {
+			return;
+		}
+		content_idx = 0;
+		setTimeout("typewriter()", speed);
+	} else {
+		target[target_idx].text(display_content + "_");
+		content_idx++;
+		setTimeout("typewriter()", speed);
+	}
+}
+
+init();
+
+typewriter();
